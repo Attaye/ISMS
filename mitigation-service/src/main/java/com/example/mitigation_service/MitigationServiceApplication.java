191e22b7e824716c6b4171a0a30d72650541ac4d
mustafa.attaye <mustafa.attaye@akkodis.com>
2025-09-01 21:18:36 +0200

    Initial commit of all services to the repo

---
+++ mitigation-service/src/main/java/com/example/mitigation_service/MitigationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mitigation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitigationServiceApplication.class, args);
+	}
+
+}
